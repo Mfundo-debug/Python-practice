359efc65607f72eae40db37087dd6e48db546212
Mfundo-debug <diditmfundo@gmail.com>
2023-10-16 00:35:17 +0200

    test 2/3 passed

---
+++ linear_algebra.py
@@ -0,0 +1,17 @@
+"""
+Task
+
+You are given a square matrix A with dimensions NXN. 
+Your task is to find the determinant. Note: Round the answer to 2 places after the decimal.
+
+Input Format
+
+The first line contains the integer N.
+The next N lines contains the N space separated elements of array A.
+"""
+import numpy
+
+if __name__ =='__main__':
+    n = int(input())
+    a = numpy.array([input().split() for _ in range(n)], float)
+    print(numpy.linalg.det(a))
