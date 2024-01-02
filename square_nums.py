eff91951e265ef6dbfe4a6494a222b61b04b89cd
Mfundo-debug <diditmfundo@gmail.com>
2024-01-02 04:47:11 +0200

    Add sum of squared numbers to output

--- square_nums.py
+++ square_nums.py
@@ -7,6 +7,7 @@ if __name__ == "__main__":
         user_input = input("Enter a list of numbers (separated by commas): ")
         numbers = [int(x) for x in user_input.split(",")]
         result = square_numbers(numbers)
-        print(result)
+        print(result,"\n",
+             "sum of the squared numbers", sum(result) )
     except ValueError:
         print("Invalid input. Please enter a valid list of numbers.")
