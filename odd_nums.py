6c91898e8df89ced55017e6ef740a8d244a8e5dd
Mfundo-debug <diditmfundo@gmail.com>
2024-01-06 00:25:13 +0200

    Refactored odd_nums.py to allow user input for the range of numbers and added input validation.

--- odd_nums.py
+++ odd_nums.py
@@ -1,3 +1,12 @@
-x = [1,2,3,4,5,6,7,8,9,10]
-odd_nums = [x for x in x if x%2!=0]
-print(odd_nums)
+# Get user input for the range of numbers
+start = int(input("Enter the starting number: "))
+end = int(input("Enter the ending number: "))
+
+# Validate the input
+if start >= end:
+    print("Invalid input. The starting number should be less than the ending number.")
+else:
+    # Generate the list of odd numbers
+    x = list(range(start, end+1))
+    odd_nums = [num for num in x if num % 2 != 0]
+    print(odd_nums)
