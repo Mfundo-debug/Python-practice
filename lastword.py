b0acb47cfb5dda90da03ef2e21ef0f97636d1389
Mfundo-debug <diditmfundo@gmail.com>
2024-01-31 00:47:06 +0200

    Add function to find length of last word

--- lastword.py
+++ lastword.py
@@ -4,3 +4,10 @@ Example
 hello world
 the length of the last word from that sentence is 5.
 """
+def lastWord(s):
+    return len(s.split()[-1])
+
+if __name__ == '__main__':
+    s = input("Enter a sentence: ")
+    print("The length of the last word is", lastWord(s))
+    
