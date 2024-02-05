da3ce1d7ab5504ffb0e5909addba70294b821979
Mfundo-debug <diditmfundo@gmail.com>
2024-02-05 05:09:06 +0200

    Update numpy import in arraynumpy.py

--- arraynumpy.py
+++ arraynumpy.py
@@ -2,9 +2,9 @@
 use numpy library to compute the determinant of a matrix
 """
 def determinant_of_matrix(n, a):
-    import numpy
+    import numpy as np
     return round(numpy.linalg.det(a),2)
 if __name__ == '__main__':
     n = int(input())
-    a = numpy.array([input().split() for _ in range(n)], float)
+    a = np.array([input().split() for _ in range(n)], float)
     print(determinant_of_matrix(n, a))
