8584822ed02c1ad125bda1fee09a840e376e6ba0
Mfundo-debug <diditmfundo@gmail.com>
2024-02-06 01:23:42 +0200

    Fix import error in arraynumpy.py

--- arraynumpy.py
+++ arraynumpy.py
@@ -3,7 +3,7 @@ use numpy library to compute the determinant of a matrix
 """
 def determinant_of_matrix(n, a):
     import numpy as np
-    return round(numpy.linalg.det(a),2)
+    return round(np.linalg.det(a),2)
 if __name__ == '__main__':
     n = int(input())
     a = np.array([input().split() for _ in range(n)], float)
