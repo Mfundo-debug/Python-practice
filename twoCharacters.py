e5dba1c33e27eeb02b47ea723af20cca90b34a72
Mfundo-debug <diditmfundo@gmail.com>
2024-02-12 00:23:35 +0200

    Refactor alternate function to improve readability and optimize performance

--- twoCharacters.py
+++ twoCharacters.py
@@ -5,6 +5,11 @@ Determine the longest string possible that contains just two alternating letters
 Example 
 s = 'abaacdabd'
 Delete a, to leave abacdabd. Now, remove character c to leave the valid string bdbd with a length of 4.
+Removing either b or d at any point would not result in a valid string. Return 4.
+
+Given a string s, convert it to the longest possible string t made up only of alternating characters.
+Return the length of string t. If no string t can be formed, return 0.
+
 Function Description
 
 Complete the alternate function in the editor below.
@@ -20,21 +25,23 @@ Constraints
 s[i] belongs to ascii[a-z]
 """
 def alternate(s):
+    if len(set(s)) == 1:
+        return 0
+
     max_length = 0
     for i in range(26):
         for j in range(i+1, 26):
-            c1 = chr(97+i)
-            c2 = chr(97+j)
+            c1 = chr(i + 97)
+            c2 = chr(j + 97)
             last = ''
             length = 0
             for c in s:
                 if c == c1 or c == c2:
-                    if c != last:
-                        length += 1
-                        last = c
-                    else:
+                    if c == last:
                         length = 0
                         break
+                    last = c
+                    length += 1
             max_length = max(max_length, length)
     return max_length
     
