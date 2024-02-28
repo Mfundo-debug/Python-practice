478947f240bd52ed2e8ab27f397e800473f76505
Mfundo-debug <diditmfundo@gmail.com>
2024-02-28 04:29:32 +0200

    Add MorganString.py with morganAndString function

---
+++ MorganString.py
@@ -0,0 +1,45 @@
+"""
+Jack and Daniel are friends. Both of them like letters, especially uppercase ones.
+They are cutting uppercase letters from newspapers, and each one of them has his collection of letters stored in a stack.
+
+One beautiful day, Morgan visited Jack and Daniel. He saw their collections.
+He wondered what is the lexicographically minimal string made of those two collections. 
+He can take a letter from a collection only when it is on the top of the stack. Morgan wants to use all of the letters in their collections.
+As an example, assume Jack has collected a = [A,C,A] and Daniel has b = [B,C,F]. The examples shows the top at index 0 for each stack of letters.
+
+Function Description
+
+Complete the morganAndString function in the editor below.
+
+morganAndString has the following parameter(s):
+
+string a: Jack's letters, top at index 
+string b: Daniel's letters, top at index 
+Returns
+- string: the completed string
+Constraints
+1 <= |a|, |b| <= 10^5
+1 <= T <= 5
+a and b contain upper-case letters only, ascii[A-Z]
+"""
+
+def morganAndString(a, b):
+    res = ''
+    i = 0
+    j = 0
+    while i < len(a) or j < len(b):
+        if j >= len(b) or (i < len(a) and a[i:] < b[j:]):
+            res += a[i]
+            i += 1
+        else:
+            res += b[j]
+            j += 1
+    return res
+
+if __name__ == '__main__':
+    t = int(input().strip())
+    for t_itr in range(t):
+        a = input()
+        b = input()
+        result = morganAndString(a, b)
+        print(result)
