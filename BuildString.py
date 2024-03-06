dff6ec9acea8695b59d4e42c4fcd095d57462a34
Mfundo-debug <diditmfundo@gmail.com>
2024-03-06 06:09:43 +0200

    Refactor string building algorithm

--- BuildString.py
+++ BuildString.py
@@ -13,96 +13,59 @@ Constraints
 1 <= N <= 3*10^4
 1 <= A, B <= 10000
 """
-class Node:
-    def __init__(self, start, end=None, parent=None):
-        self.start = start
-        self.end = end
-        self.parent = parent
-        self.children = {}
+#!/bin/python3
 
-class ActivePoint:
-    def __init__(self, node, edge, length):
-        self.node = node
-        self.edge = edge
-        self.length = length
+import os
+import sys
+import re
 
-def build_suffix_tree(s):
-    root = Node(-1, -1)
-    root.children[s[0]] = Node(0, len(s), root)
-    active = ActivePoint(root, None, 0)
-    remainder = 0
-    for i in range(1, len(s)):
-        remainder += 1
-        while remainder > 0:
-            if active.length == 0:
-                if s[i] in active.node.children:
-                    active.edge = s[i]
-                    active.length += 1
-                    break
-                else:
-                    active.node.children[s[i]] = Node(i, len(s), active.node)
-                    remainder -= 1
-            else:
-                edge_start = active.node.children[active.edge].start
-                if active.length < i - edge_start:
-                    if s[edge_start + active.length] in active.node.children[active.edge].children:
-                        active.node = active.node.children[active.edge]
-                        active.edge = s[edge_start + active.length]
-                        active.length = 1
-                        break
-                    else:
-                        old_node = active.node.children[active.edge]
-                        new_node = Node(old_node.start, old_node.start + active.length, active.node)
-                        new_node.children[s[i]] = Node(i, len(s), new_node)
-                        new_node.children[s[old_node.start + active.length]] = old_node
-                        old_node.start += active.length
-                        old_node.parent = new_node
-                        active.node.children[active.edge] = new_node
-                        remainder -= 1
-                elif active.length == i - edge_start:
-                    active.node = active.node.children[active.edge]
-                    active.edge = None
-                    active.length = 0
-                else:
-                    if s[edge_start + active.length] == s[i]:
-                        active.length += 1
-                        break
-                    else:
-                        old_node = active.node.children[active.edge]
-                        new_node = Node(old_node.start, old_node.start + active.length, active.node)
-                        new_node.children[s[i]] = Node(i, len(s), new_node)
-                        new_node.children[s[old_node.start + active.length]] = old_node
-                        old_node.start += active.length
-                        old_node.parent = new_node
-                        active.node.children[active.edge] = new_node
-                        remainder -= 1
-            if active.node != root:
-                active.node = active.node.parent
-            else:
-                active.length -= 1
-                if active.length > 0:
-                    active.edge = s[i - remainder + 1]
-    return root
+#
+# Complete the buildString function below.
+#
 
 def buildString(a, b, s):
-    n = len(s)
-    root = build_suffix_tree(s + "$")
-    cost = [0]*(n+1)
-    for i in range(1, n+1):
-        cost[i] = cost[i-1] + a
-        node = root
-        j = i
-        while j < n and s[j] in node.children:
-            node = node.children[s[j]]
-            edge_length = min(node.end, n+1) - node.start
-            if i + edge_length <= n:
-                cost[i+edge_length] = min(cost[i+edge_length], cost[i-1] + b)
-            j += edge_length
-    return cost[n]
+    N = len(s)
 
-if __name__ == "__main__":
+    cost = [0]*N
+
+    cost[0] = a
+    index = 1
+    for i in range(1,N):
+        if s[index:i+1] not in s[:index] or index == -1:
+            index = findLongestOccurence(s[:i+1], i)
+        if index==-1:
+            cost[i] = cost[i-1] + a
+        else:
+            cost[i] = min(cost[i-1]+a, cost[index-1]+b)
+    return cost[-1]
+
+def findLongestOccurence(s, index):
+    indexHasChanged = False
+    while s[index:] in s[:index]:
+        index -= 1
+        indexHasChanged = True
+    return -1 if not indexHasChanged else index + 1
+
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
     t = int(input())
-    for _ in range(t):
-        n,a,b = map(int,input().split())
+
+    for t_itr in range(t):
+        nab = input().split()
+
+        n = int(nab[0])
+
+        a = int(nab[1])
+
+        b = int(nab[2])
+
         s = input()
-        print(buildString(a,b,s))
+        
+        result =  buildString(a, b, s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
