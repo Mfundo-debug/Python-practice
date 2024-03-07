8dcc1d91d26ca4be28961aa309993d2d474dffc2
Mfundo-debug <diditmfundo@gmail.com>
2024-03-07 06:46:57 +0200

    REAL SOLUTION

--- AshtonString.py
+++ AshtonString.py
@@ -19,21 +19,28 @@ Constraints
 Eacch character of string s is a lowercase English letter ascii.
 k will be an appropriate value for the given string s.
 """
-def ashtonString(s, k):
-    n = len(s)
-    substrings = []
-    for i in range(n):
-        for j in range(i+1, n+1):
-            substrings.append(s[i:j])
-    substrings.sort()
-    print(substrings)
-    concatenated = ''.join(substrings)
-    return concatenated[k-1]
+def foo(text, k):
+    stack = [("",list(range(len(text))))]
+    while stack != []:
+        prefix,ii = stack.pop()
+        if k<len(prefix):
+            return prefix[k]
+        k -= len(prefix)
+        cs = sorted([(text[i],i+1) for i in ii if i<len(text)], reverse=True)
+        i = 0
+        while i<len(cs):
+            c = cs[i][0]
+            ii2 = [cs[i][1]]
+            j = i+1
+            while j<len(cs) and cs[j][0]==c:
+                ii2.append(cs[j][1])
+                j+=1
+            stack.append((prefix+c, ii2))
+            i=j
+    return None
 
-if __name__ == '__main__':
-    t = int(input().strip())
-    for t_ir in range(t):
-        s = input().strip()
-        k = int(input().strip())
-        result = ashtonString(s, k)
-        print(result)
+T = int(input().strip())
+for i in range(T):
+    text = input().strip()
+    k = int(input().strip())
+    print(foo(text,k-1))
