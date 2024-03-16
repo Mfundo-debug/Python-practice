a0e15fc3236e23f791c483826925aa766caabed3
Mfundo-debug <diditmfundo@gmail.com>
2024-03-16 06:29:26 +0200

    Add heapsearch algorithm implementation

---
+++ heapsearch.py
@@ -0,0 +1,17 @@
+"""
+implementation of heapsearch algorithm
+"""
+import heapq
+
+def heap_search(lst, item):
+    heapq.heapify(lst)
+    while lst:
+        if lst[0] == item:
+            return True
+        heapq.heappop(lst)
+    return False
+
+if __name__ == "__main__":
+    lst = [3, 2, 5, 1, 4]
+    print(heap_search(lst, 3))  #
+    print(heap_search(lst, 6))  
