f5537b7438f7cd94a98bd1768e709989ce16c77a
Mfundo-debug <diditmfundo@gmail.com>
2024-03-18 18:34:25 +0200

    shift index

--- shiftindex.py
+++ shiftindex.py
@@ -1,6 +1,7 @@
 """
 Given an array [1,2,-3,0,0,5,4], shift zeros to the initial part of the array and shift non-zeros to the end of the array. 
 The result should be [0,0,1,2,-3,5,4].
+
 """
 def shiftindex(arr):
     i = 0
