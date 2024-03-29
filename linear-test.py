d44def1c036ce87cb1460a93b4f7eea6013f4f56
Mfundo-debug <diditmfundo@gmail.com>
2024-03-29 08:35:20 +0200

    Add linear regression model and plot with uncertainty and R^2 value

---
+++ linear-test.py
@@ -0,0 +1,31 @@
+"""
+Write a script that models data points and fit the linear regression and plots it.
+- Make sure to add the uncertainty in the graph +_2 standard deviation
+- Make sure to add the R^2 value in the graph
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+# Generate some data
+np.random.seed(0)
+x = np.random.rand(100, 1)
+y = 2 + 3 * x + np.random.rand(100, 1)
+
+# Fit the model
+model = LinearRegression()
+model.fit(x, y)
+y_pred = model.predict(x)
+
+# Plot the data
+plt.figure(figsize=(10, 5))
+plt.scatter(x, y, s=10)
+plt.plot(x, y_pred, color='r')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Linear Regression')
+plt.show()
+
+# Print the R^2 value
+print('R^2:', r2_score(y, y_pred))
