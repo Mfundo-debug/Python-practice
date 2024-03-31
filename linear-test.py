1d789106c84038215eb8abc0f9ff49c799aaa29e
Mfundo-debug <diditmfundo@gmail.com>
2024-03-31 07:47:55 +0200

    linear reg test

--- linear-test.py
+++ linear-test.py
@@ -9,6 +9,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
 # Generate some data
+
 np.random.seed(0)
 x = np.random.rand(100, 1)
 y = 2 + 3 * x + np.random.rand(100, 1)
