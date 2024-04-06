7947ab6be89e47d22603bc395f46f0f05e6d483f
Mfundo-debug <diditmfundo@gmail.com>
2024-04-06 07:41:27 +0200

    Add search.py with electric_field function

---
+++ search.py
@@ -0,0 +1,20 @@
+"""
+Make use of maxwell's equations to find the electric field of a point charge.
+use the following constants:
+k = 8.99e9
+q = 1e-9
+r = 1
+"""
+import math
+import numpy as np
+
+def electric_field(q, r):
+    k = 8.99e9
+    E = k*q/r**2
+    return E
+
+if __name__ == '__main__':
+    q = 1e-9
+    r = 1
+    E = electric_field(q, r)
+    print(f'The electric field of a point charge is {E}')
