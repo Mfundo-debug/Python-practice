b0023beaf2b169d326d1e290eed66d85b629308f
Mfundo-debug <diditmfundo@gmail.com>
2024-04-08 17:06:14 +0200

    Add maxwell_algorithm function to find the shortest path between two points in a graph

---
+++ maxwell.py
@@ -0,0 +1,60 @@
+"""
+use maxwell algorithm to search for the shortest path between two points in a graph
+in a graph, the shortest path between two points is the path with the minimum number of edges
+"""
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+
+def maxwell_algorithm(graph, start, end):
+    """
+    Find the shortest path between two points in a graph using maxwell's equations.
+    """
+    # Initialize the shortest path
+    shortest_path = []
+    # Initialize the queue
+    queue = []
+    # Add the start node to the queue
+    queue.append([start])
+    # Loop until the queue is empty
+    while queue:
+        # Get the first path from the queue
+        path = queue.pop(0)
+        # Get the last node from the path
+        node = path[-1]
+        # Check if the node is the end node
+        if node == end:
+            # Update the shortest path
+            shortest_path = path
+            break
+        # Loop through the neighbors of the node
+        for neighbor in graph[node]:
+            # Check if the neighbor has not been visited
+            if neighbor not in path:
+                # Create a new path
+                new_path = list(path)
+                new_path.append(neighbor)
+                # Add the new path to the queue
+                queue.append(new_path)
+    return shortest_path
+
+if __name__ == '__main__':
+    # Create a graph
+    graph = {
+        'A': ['B', 'C'],
+        'B': ['A', 'D', 'E'],
+        'C': ['A', 'F'],
+        'D': ['B'],
+        'E': ['B', 'F'],
+        'F': ['C', 'E']
+    }
+    # Create a networkx graph
+    G = nx.Graph(graph)
+    # Draw the graph
+    nx.draw(G, with_labels=True)
+    plt.show()
+    # Find the shortest path between two points
+    start = 'A'
+    end = 'F'
+    shortest_path = maxwell_algorithm(graph, start, end)
+    print(f'The shortest path between {start} and {end} is {shortest_path}')
