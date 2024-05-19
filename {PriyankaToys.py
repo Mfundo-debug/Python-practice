3d219f25f9b158251ef849962dd81c43406fd5ee
Mfundo-debug <diditmfundo@gmail.com>
2024-05-19 18:14:32 +0200

    Refactor code to optimize sorting and comparison in PriyankaToys.py

--- {PriyankaToys.py
+++ {PriyankaToys.py
@@ -14,7 +14,7 @@ toys has the following parameter(s):
 w: an array of integers that represent the weights of each order to ship
 
 Constraints:\\
-    
+
 1 <= n <= 10^5
 0 <= w[i] <= 10^4, where 0 <= i < n
 
