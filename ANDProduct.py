b01ae4749eba1f36d186db8e1e764f9934003ea9
Mfundo-debug <diditmfundo@gmail.com>
2024-06-05 18:01:52 +0200

    chore: Refactor ANDProduct.py for bitwise AND calculations in a range

--- ANDProduct.py
+++ ANDProduct.py
@@ -3,6 +3,7 @@ Consider two non-negative long integers, a and b, where a <= b. We define the bi
 the inclusive range between a and b is [a, a+1, a+2, ..., b-1, b] where '&' represents the bitwise AND operation.
 Given n pairs of long integers, a[i] and b[i], compute and print the bitwise AND of all natural numbers in the inclusive range between a[i] and b[i].
 For example, if a = 10 and b = 14, the bitwise AND of the inclusive range [10, 11, 12, 13, 14] is 8.
+
 Function Description
 
 Complete the andProduct in the editor below. It should return the computed value as an integer.
