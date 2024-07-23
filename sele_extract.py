63329de8899eaa965f69349b167ef3a7de6f508a
Mfundo-debug <diditmfundo@gmail.com>
2024-07-23 18:22:23 +0200

    chore: Update selenium script to load PDF filler form

--- sele_extract.py
+++ sele_extract.py
@@ -10,7 +10,6 @@ driver = webdriver.Chrome(options=options)
 
 url = 'https://www.pdffiller.com/jsfiller-desk12/?mode=ref&lang=en&ref=https%3A%2F%2Fwww.pdffiller.com%2Fen%2Fforms%2Fdashboard&projectId=1341383752&loader=tips&MEDIUM_PDFJS=true&PAGE_REARRANGE_V2_MVP=true&richTextFormatting=true&isPageRearrangeV2MVP=true&jsf-page-rearrange-v2=true&jsf-new-header=false&routeId=01e4b4bc6e38052ce472779d65e49acf#340eb4730c8342cf9d5e20a072e34f77'
 driver.get(url)
-
 # Wait for the iframe to load
 wait = WebDriverWait(driver, 10)
 iframe = wait.until(EC.presence_of_element_located((By.ID, 'jsf-pdf-viewer')))
