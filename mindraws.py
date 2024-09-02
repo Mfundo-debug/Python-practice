e0d2020b8dd1d3ea8ba6acc6c8162332612338b4
Mfundo-debug <diditmfundo@gmail.com>
2024-09-02 18:52:41 +0200

    Refactor mindraws.py for improved efficiency and readability

--- mindraws.py
+++ mindraws.py
@@ -25,6 +25,7 @@ def maximumDraws(n):
 
 if __name__ == '__main__':
     t = int(input().strip())
+    
     for t_itr in range(t):
         n = int(input().strip())
         result = maximumDraws(n)
