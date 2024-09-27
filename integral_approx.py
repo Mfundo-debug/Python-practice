389afbb30d236b3bbc06256d81e3e0ebc442b87e
Mfundo-debug <diditmfundo@gmail.com>
2024-09-27 19:48:16 +0200

    Refactor integral_approx.py to generate random samples for x and y and compute the average of the integrand values

--- integral_approx.py
+++ integral_approx.py
@@ -15,9 +15,11 @@ x_samples = np.random.uniform(0, 1, N)
 y_samples = np.random.uniform(0, 1, N)
 
 # evaluate the integrand at the random samples
+
 integrand_values = integrand(x_samples, y_samples)
 
 # compute the average of the integrand values
+
 integral_approx = np.mean(integrand_values)
 
 # since we are integrating over a unit square , the area is 1
